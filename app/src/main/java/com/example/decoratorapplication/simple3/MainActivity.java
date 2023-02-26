477d98cd71ed0bf2140dff673de924443a0f9f78
yangjinghua <1692047690@qq.com>
2023-02-26 12:32:45 +0800

    装饰设计模式完结

--- app/src/main/java/com/example/decoratorapplication/simple3/MainActivity.java
+++ app/src/main/java/com/example/decoratorapplication/simple3/MainActivity.java
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.decoratorapplication.R;
 import com.example.decoratorapplication.simple1.TeacherEat;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,5 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TeacherEat teacherEat = new TeacherEat();
         teacherEat.eat();
+
+
+        try {
+            FileReader fileReader = new FileReader("xxxx.file");
+            BufferedReader bfr = new BufferedReader(fileReader);
+            bfr.readLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
